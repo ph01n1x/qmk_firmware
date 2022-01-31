76be2c7ca09e99a005a8fe793ce55444650c49b4
ph01n1x <ph01n1x@vitsco.com>
2022-01-31 01:46:24 -0500

    Squashed commit of the following:
    commit 5b605576965b6ae806ef6437c2cbd98166804d56
    Author: ph01n1x <ph01n1x@vitsco.com>
    Date:   Mon Jan 31 01:18:37 2022 -0500
    
        Zodiark customization

--- keyboards/aleblazer/zodiark/config.h
+++ keyboards/aleblazer/zodiark/config.h
@@ -56,7 +56,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGBLIGHT_SPLIT
 #define RGBLED_SPLIT { 34, 34 }
 #define RGBLIGHT_LIMIT_VAL 170
-#define RGBLIGHT_ANIMATIONS
+//#define RGBLIGHT_ANIMATIONS
 #define RGBLIGHT_HUE_STEP 8
 #define RGBLIGHT_SAT_STEP 8
 #define RGBLIGHT_VAL_STEP 8
---
+++ keyboards/aleblazer/zodiark/keymaps/vits/config.h
@@ -0,0 +1,27 @@
+/*
+Copyright 2021 Spencer Deven <splitlogicdesign@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#pragma once
+
+#ifdef OLED_ENABLE
+  #define OLED_DISPLAY_128X64
+  #define OLED_TIMEOUT 400000
+#endif
+
+#define SPLIT_LED_STATE_ENABLE
+#define SPLIT_OLED_ENABLE
+
+#define RGBLIGHT_EFFECT_RAINBOW_SWIRL
